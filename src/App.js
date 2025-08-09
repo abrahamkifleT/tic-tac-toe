21b7a45ec15d805a0d23ba2050619ef6503c0f55
abrahamkifleT <abrahamkifle1212@gmail.com>
2025-08-10 02:58:35 +0300

    fix: update currentSquares to reflect the current move in game state

--- src/App.js
+++ src/App.js
@@ -77,7 +77,7 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquare) {
     const nextHistory = [...history, nextSquare];
