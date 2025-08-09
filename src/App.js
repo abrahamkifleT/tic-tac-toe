1836b06a5739b4b0822fd0839ceefefae63472ee
abrahamkifleT <abrahamkifle1212@gmail.com>
2025-08-09 12:13:25 +0300

    import react and set the value x when each time the squere is clicked.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-function Square({ value }) {
+import React, { useState } from "react";
+
+function Square() {
   const [value, setValue] = useState(null);
 
   function handleClick() {
-    console.log("clicked");
+    setValue("X");
   }
   return (
     <button className="square" onClick={handleClick}>
